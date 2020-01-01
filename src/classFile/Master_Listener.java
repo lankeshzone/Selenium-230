f5b6c46817ab01b67ff1def2353a6392df7bd182
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2020-01-01 15:54:47 +0530

    modified

--- src/classFile/Master_Listener.java
+++ src/classFile/Master_Listener.java
@@ -1,72 +1,66 @@
 package classFile;
 
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 
-	import java.io.File;
-	import java.io.IOException;
+import classFile.Login;
 
-	import org.apache.commons.io.FileUtils;
-	import org.openqa.selenium.OutputType;
-	import org.openqa.selenium.TakesScreenshot;
-	import org.openqa.selenium.WebDriver;
-	import org.testng.ITestListener;
-	import org.testng.ITestResult;
-	import org.testng.Reporter;
+public class Master_Listener implements ITestListener
+{
+	
 
-	import classFile.Login;
-
-	public class Master_Listener implements ITestListener
+	public void onTestFailure(ITestResult result) 
 	{
+          String rs = getMethodContext(result);
+          System.err.println(rs + " | FAILED");
+          Reporter.log(rs + " | FAILED");
+	}
+
+    public void onTestSkipped(ITestResult result) 
+    {
+          String rs = getMethodContext(result);
+          System.out.println(rs + " | SKIPPED");
+          Reporter.log(rs + " | SKIPPED");
+    }
+
+
+    private String getMethodContext(ITestResult result) 
+    {
+          //String browser = result.getTestContext().getCurrentXmlTest()
+          //.getParameter("browser");
+          String testClasss = result.getTestClass().getName();
+          String name = result.getName();
+          String rs = testClasss + " | " + name;
+          return rs;
+    }
+
+	
+	public void onTestSuccess(ITestResult result)
+	{
+		System.out.println(result.getMethod());
 		
-
-		 public void onTestFailure(ITestResult result) 
-	       {
-	              String rs = getMethodContext(result);
-	              System.err.println(rs + " | FAILED");
-	              Reporter.log(rs + " | FAILED");
-	       }
-
-	       public void onTestSkipped(ITestResult result) 
-	       {
-	              String rs = getMethodContext(result);
-	              System.out.println(rs + " | SKIPPED");
-	              Reporter.log(rs + " | SKIPPED");
-	       }
-
-
-	       private String getMethodContext(ITestResult result) 
-	       {
-	              //String browser = result.getTestContext().getCurrentXmlTest()
-	              //.getParameter("browser");
-	              String testClasss = result.getTestClass().getName();
-	              String name = result.getName();
-	              String rs = testClasss + " | " + name;
-	              return rs;
-	       }
-		
-		
-		public void onTestSuccess(ITestResult result)
+		Login l = new Login();
+		try
 		{
-			System.out.println(result.getMethod());
+		l.screenshot(result);
+		}
+		catch(IOException e)
+		{
 			
-			Login l = new Login();
-			try
-			{
-			l.screenshot(result);
-			}
-			catch(IOException e)
-			{
-				
-			}
-			
-			String rs = getMethodContext(result);
-            System.out.println(rs + " | PASSED");
-            Reporter.log(rs + " | PASSED");
 		}
 		
-		
-		
-		
+		String rs = getMethodContext(result);
+        System.out.println(rs + " | PASSED");
+        Reporter.log(rs + " | PASSED");
 	}
+}
 
 
