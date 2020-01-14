e1ebae0ff0cb9f8d9cafbddcdccc32dade6857f4
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2020-01-14 22:48:45 +0530

    also flights

--- src/Tests/Flights.java
+++ src/Tests/Flights.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-import PageObject.HomePage;
 import PageObject.signOn;
 
 public class Flights {
